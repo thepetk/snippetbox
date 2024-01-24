de6c6510f8f027855b3c8502847724742a9f8d83
thepetk <thepetk@gmail.com>
2024-01-24 14:43:06 +0000

    Update models with transactions
    Signed-off-by: thepetk <thepetk@gmail.com>

--- cmd/models/models.go
+++ cmd/models/models.go
@@ -3,13 +3,14 @@ package models
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"time"
-
-	"github.com/gofrs/uuid"
 )
 
+var ErrNoRecord = errors.New("models: no matching record found")
+
 type Snippet struct {
-	ID      string    `json:"id"`
+	ID      int       `json:"id"`
 	Title   string    `json:"title,omitempty"`
 	Content string    `json:"content,omitempty"`
 	Created time.Time `json:"created"`
@@ -21,51 +22,77 @@ type SnippetService interface {
 	Create(snippet *Snippet) error
 }
 
-type PostgresSnippetService struct {
+type SnippetModel struct {
 	db           *sql.DB
 	queryTimeout time.Duration
 }
 
-func (p *PostgresSnippetService) Create(snippet *Snippet) error {
+func (s *SnippetModel) SetDB(db *sql.DB) {
+	s.db = db
+	s.queryTimeout = 5 * time.Second
+}
+
+func (s *SnippetModel) Create(snippet *Snippet) (int, error) {
 	query := `
         INSERT INTO snippets (title, content, created, expires) 
         VALUES ($1, $2, current_timestamp, current_timestamp + '365 days')
         RETURNING id, title, created`
 
-	newV4, err := uuid.NewV4()
+	ctx, cancel := context.WithTimeout(context.Background(), s.queryTimeout)
+	defer cancel()
+
+	err := s.db.QueryRowContext(ctx, query, snippet.Title, snippet.Content).
+		Scan(&snippet.ID, &snippet.Title, &snippet.Created)
+
 	if err != nil {
-		return err
+		return 0, err
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), p.queryTimeout)
-	defer cancel()
-
-	return p.db.QueryRowContext(ctx, query, newV4.String(), snippet.Title, snippet.Content).
-		Scan(&snippet.ID, &snippet.Title, &snippet.Created)
+	return snippet.ID, err
 }
 
-func (p *PostgresSnippetService) Get(id string) (*Snippet, error) {
-	query := `
-        SELECT id, title, content, created, expires
-        FROM snippets
-        WHERE id = $1`
+func (s *SnippetModel) Get(id string) (*Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+		WHERE expires > current_timestamp AND id=$1`
 
-	var snippet Snippet
+	row := s.db.QueryRow(stmt, id)
 
-	ctx, cancel := context.WithTimeout(context.Background(), p.queryTimeout)
-	defer cancel()
+	sn := &Snippet{}
 
-	err := p.db.QueryRowContext(ctx, query, id).Scan(
-		&snippet.ID,
-		&snippet.Title,
-		&snippet.Content,
-		&snippet.Created,
-		&snippet.Expires,
-	)
+	err := row.Scan(&sn.ID, &sn.Title, &sn.Content, &sn.Created, &sn.Expires)
+	if err == sql.ErrNoRows {
+		return nil, ErrNoRecord
+	} else if err != nil {
+		return nil, err
+	}
 
+	return sn, nil
+}
+
+func (s *SnippetModel) Latest() ([]*Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+	WHERE expires > current_timestamp ORDER BY created DESC LIMIT 10`
+
+	rows, err := s.db.Query(stmt)
 	if err != nil {
 		return nil, err
 	}
 
-	return &snippet, nil
+	defer rows.Close()
+
+	snippets := []*Snippet{}
+	for rows.Next() {
+		sn := &Snippet{}
+		err = rows.Scan(&sn.ID, &sn.Title, &sn.Content, &sn.Created, &sn.Expires)
+		if err != nil {
+			return nil, err
+		}
+		snippets = append(snippets, sn)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
