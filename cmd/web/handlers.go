c24bc0f98ebe062d17feed67405555486542f5f3
thepetk <thepetk@gmail.com>
2024-01-25 07:06:20 +0000

    Finalize handles and helpers
    Signed-off-by: thepetk <thepetk@gmail.com>

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,36 +9,22 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-	// if r.URL.Path != "/" {
-	// 	app.notFound(w)
-	// 	return
-	// }
-	// files := []string{
-	// 	"./ui/html/home.page.tmpl",
-	// 	"./ui/html/base.layout.tmpl", "./ui/html/footer.partial.tmpl",
-	// }
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// 	return
-	// }
-	// err = ts.Execute(w, nil)
-	// if err != nil {
-	// 	app.serverError(w, err)
-	// }
 	if r.URL.Path != "/" {
 		app.notFound(w)
 		return
 	}
+
 	s, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
-	for _, snippet := range s {
-		fmt.Fprintf(w, "%v\n", snippet)
-	}
+
+	app.render(w, r, "home.page.tmpl", &templateData{
+		Snippets: s,
+	})
 }
+
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
@@ -53,9 +39,12 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
-	// Write the snippet data as a plain-text HTTP response body.
-	fmt.Fprintf(w, "%v", s)
+
+	app.render(w, r, "show.page.tmpl", &templateData{
+		Snippet: s,
+	})
 }
+
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		w.Header().Set("Allow", "POST")
